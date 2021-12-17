027f15571b48be7494a85da982aebac54554ba94
Volodymyr Shyshko <vovashishko253@gmail.com>
2021-12-17 19:43:30 +0200

    testing_servise

--- service/service/settings.py
+++ service/service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+#import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-88iz)3_t@)i94*!s6fk2v)1tgkoe#j-p21nexknt$9idx4dgpw'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
